f56ed61fb94c5d7ce9146cdaf173b6fff23b1bb7
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-12-20 11:06:09 -0300

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import StackNavigator from "./StackNavigator";
+import Profile from "../screens/Profile";
+import Logout from "../screens/Logout";
+import firebase from "firebase";
+
+import CustomSidebarMenu from "../screens/CustomSidebarMenu";
+
+const Drawer = createDrawerNavigator();
+
+export default class DrawerNavigator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            light_theme: true
+        };
+    }
+
+    componentDidMount() {
+       //Insira o código aqui
+    }
+
+    render() {
+       //Insira o código aqui        
+    }
+}
